5fd2a50a69baeb5b458d2f89da8291b7bb97e081
TB <tatianabertazoli@gmail.com>
2022-11-30 21:41:22 -0600

    added env file

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,34 @@
 const mysql = require('mysql2'); // to import mysql
+require('dotenv').config();
 
   // Connect to database
-function connectDB() {
- return mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'CsCb$1se',
-      database: 'employee_tracker'
-    },    
-  );
-}
+// function connectDB() {
+//  return mysql.createConnection(
+//     {
+//       host: 'localhost',
+//       // Your MySQL username,
+//       user: 'root',
+//       // Your MySQL password
+//       password: 'CsCb$1se',
+//       database: 'employee_tracker'
+//     },    
+//   );
+// }
 
-const db = connectDB();
+
+
+// module.exports = db;
+ 
+  const db =  mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: process.env.DB_NAME,
+  password: process.env.DB_PW 
+})
+
+
+
 
 module.exports = db;
- 
+
 
