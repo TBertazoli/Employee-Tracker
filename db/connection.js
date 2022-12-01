692607e0445259677803eb1e6303ebfb95dc3553
TB <tatianabertazoli@gmail.com>
2022-11-30 21:42:23 -0600

    deleted password

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,7 @@
 const mysql = require('mysql2'); // to import mysql
 require('dotenv').config();
 
-  // Connect to database
-// function connectDB() {
-//  return mysql.createConnection(
-//     {
-//       host: 'localhost',
-//       // Your MySQL username,
-//       user: 'root',
-//       // Your MySQL password
-//       password: 'CsCb$1se',
-//       database: 'employee_tracker'
-//     },    
-//   );
-// }
 
-
-
-// module.exports = db;
- 
   const db =  mysql.createConnection({
   host: 'localhost',
   user: 'root',
